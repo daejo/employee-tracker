97c7c37988419e9633e139fa127811ea131b4a6d
daejo <daejo@icloud.com>
2020-10-08 16:51:19 -0700

    installs dependencies

---
+++ server.js
@@ -0,0 +1,13 @@
+  //* TYPE IN TERMINAL FOR: */
+      // checking if you're in the root directory of project ||  sqlite3 --version
+      // to create election database ||  sqlite3 db/election.db
+      // to quit SQLITE ||  .quit
+      // to run SQLITE ||  sqlite3
+      // to open data base ||  .open db/election.db
+      // to see tables of a database || .schema
+      // to print column headers ||  .headers on  ||  .mode column
+  //* INSTALL PACKAGES */
+      // Initilizes Node.js ||  npm init --y
+      // Creates .gitignore and adds node_modules inside ||  echo "node_modules/" > .gitignore
+      // Installs express ||  npm install express
+      // Installs jest ||  npm install jest --save-dev
