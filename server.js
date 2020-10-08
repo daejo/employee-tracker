e07b90ca4af67a9992231db3c5001f0dff6ce39e
daejo <daejo@icloud.com>
2020-10-08 16:54:59 -0700

    sets up server.js

--- server.js
+++ server.js
@@ -11,3 +11,31 @@
       // Creates .gitignore and adds node_modules inside ||  echo "node_modules/" > .gitignore
       // Installs express ||  npm install express
       // Installs jest ||  npm install jest --save-dev
+
+      const express = require('express');
+      const db = require('./db/database');
+      const PORT = process.env.PORT || 3001;
+      const app = express();
+      
+      const apiRoutes = require('./routes/apiRoutes');
+      
+      
+      // EXPRESS MIDDLEWARE//
+      // ======================= //
+      app.use(express.urlencoded({ extended: false }));
+      app.use(express.json());
+      // ======================= //
+      
+      app.use('/api', apiRoutes);
+      
+      // Default response for any other requests(Not Found) Catch all
+      app.use((req, res) => {
+        res.status(404).end();
+      });
+      
+      // Start server after DB connection
+      db.on('open', () => {
+          app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+          });
+        });
