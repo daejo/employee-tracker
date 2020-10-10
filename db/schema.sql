ceeb74d95899f431fe84fc61af4107e2a83817bd
daejo <daejo@icloud.com>
2020-10-10 00:47:07 -0700

    adds more option in inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,23 @@
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS department;
-
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER UNSIGNED NOT NULL,
-  manager_id INTEGER 
-);
-
-CREATE TABLE role (
-  id INTEGER PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id  INTEGER
-);
-
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employee;
+USE employee;
 CREATE TABLE department (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
+);
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL
+    -- CONSTRAINT fk_department FOREIGN KEY (department_id ) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT 
+    -- CONSTRAINT fk_role FOREIGN KEY (role_id ) REFERENCES role(id) ON DELETE CASCADE,
+    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id ) REFERENCES employee(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
-INSERT INTO employee (first_name, last_name, role_id, manger_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
   ('Ronald', 'Firbank', 1, 1),
   ('Virginia', 'Woolf', 1, 1),
-  ('Piers', 'Gaveston', 1, 0),
+  ('Piers', 'Gaveston', 1, NULL),
   ('Charles', 'LeRoi', 2, 1),
   ('Katherine', 'Mansfield', 2, 1),
-  ('Dora', 'Carrington', 3, 0),
-  ('Edward', 'Bellamy', 3, 0),
+  ('Dora', 'Carrington', 3, NULL),
+  ('Edward', 'Bellamy', 3, NULL),
   ('Montague', 'Summers', 3, 1),
   ('Octavia', 'Butler', 3, 1),
   ('Unica', 'Zurn', 3, 1);
